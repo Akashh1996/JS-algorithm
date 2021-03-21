ad110b604fe2e1c8edd8ab8a2c8a359512445643
Akash Sapkota <akashh.sapkota@gmail.com>
2021-03-21 17:24:02 +0100

    highest occurence in array method

--- highestOccurence.js
+++ highestOccurence.js
@@ -1,11 +1,10 @@
-const array = [2,1,3,2,1,2,1,2,2,1]
+const array = ["b","b","a","b","d","a"]
 
 function highestOccurence(array) {
     let obj = {}
     array.map((num)=>  obj[num] ? obj[num] ++ : obj[num] = 1) 
-    return Object.fromEntries(Object.entries(obj).
-        filter(([key, value]) => value === Math.max(...Object.values(obj))))   
+    return Object.entries(Object.fromEntries(Object.entries(obj).
+        filter(([key, value]) => value === Math.max(...Object.values(obj)))))[0]
 }
-
-
-
+ 
+console.log(highestOccurence(array))
