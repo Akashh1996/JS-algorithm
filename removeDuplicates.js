44a2025faca91f3c926ebc44b0e0708d897e1f9c
Akash Sapkota <akashh.sapkota@gmail.com>
2021-03-21 12:41:08 +0100

    add three methods to remove duplicate

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,8 +1,26 @@
 const numbers = [1,2,1,,2,3,5]
-
 function removeDuplicate(num) {
    return num.filter((n, index)=> num.indexOf(n) === index)
-   
 }
 
-console.log(removeDuplicate(numbers))
+function removeDuplicate1(array) {
+   let newArray = []
+   array.map(element => {
+      if(!newArray.includes(element)){
+         newArray.push(element)
+      }
+   });
+   return newArray
+}
+
+function removeDuplicate2(array) {
+   let newArray = []
+   array.map((element)=>{
+      if(newArray.indexOf(element) === -1){
+         newArray.push(element)
+      }
+   })
+   return newArray
+
+}
+console.log(removeDuplicate2(numbers))
